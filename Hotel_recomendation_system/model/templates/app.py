eee86a634b268581096e2fbde80bb9e576f8dffa
SatadalS <satadals121@gmail.com>
2024-01-03 04:47:41 +0100

    Webpage updated, with basic hotel list format.

--- Hotel_recomendation_system/model/templates/app.py
+++ Hotel_recomendation_system/model/templates/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import pickle
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html'
+                           )
 
 if __name__ == '__main__' :
     app.run(debug=True)
